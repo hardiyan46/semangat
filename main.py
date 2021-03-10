287db50fe74cf86438c49caa20b255c7d0d11e3a
hardiyan.purnama.hp@gmail.com <smart_m07>
2021-03-11 00:14:21 +0700

    membuat frame

---
+++ main.py
@@ -0,0 +1,11 @@
+#import library wxpython
+import wx
+
+#Buat Objek Applikasi
+myapp = wx.app()
+
+#inisialisasi frame
+init_frame = wx.Frame(parent=None, title='Halo coy, semangat ya')
+
+#munculkan frame
+init_frame.Show()
