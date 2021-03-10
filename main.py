7c31ba1d745b738aecc969f250aa535160a14dc0
hardiyan.purnama.hp@gmail.com <smart_m07>
2021-03-11 00:23:06 +0700

    penambahan class

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 #import library wxpython
 import wx
+import operator
 
 #Buat Objek Applikasi
-myapp = wx.app()
+myapp = wx.App()
 
 #inisialisasi frame
 init_frame = wx.Frame(parent=None, title='Halo coy, semangat ya')
 
-#munculkan frame
+#munculkan framenya
 init_frame.Show()
+
+#looping objek nya
+myapp.MainLoop()
+
+class semangat(wx.Frame):
+    def __init__(self, parent, title):
+        super(semangat, self).__init__(parent, title=title)
+        self.Show()
+def main()
+
+
